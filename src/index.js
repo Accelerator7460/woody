e07d20945539ed229d8f2732613785ff5f96e17b
Accelerator7460 <34483048+Accelerator7460@users.noreply.github.com>
2018-09-24 12:48:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
-
+//lll
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
